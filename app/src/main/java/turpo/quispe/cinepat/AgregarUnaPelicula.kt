4181758002a39babaf42c18b7eae847f8843a91b
PattyTurpo <71868083@est.unap.edu.pe>
2024-06-02 00:06:35 -0500

    cambios

--- app/src/main/java/turpo/quispe/cinepat/AgregarUnaPelicula.kt
+++ app/src/main/java/turpo/quispe/cinepat/AgregarUnaPelicula.kt
@@ -1,32 +1,16 @@
 package turpo.quispe.cinepat
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.concurrent.thread
-import kotlin.system.measureTimeMillis
 
-class MainActivity : AppCompatActivity() {
+class AgregarUnaPelicula : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        val splashScreen=installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        splashScreen.setKeepOnScreenCondition{true}
-
-        val intent=Intent(this,MenuActivity::class.java)
-        startActivity(intent)
-
-
+        setContentView(R.layout.activity_agregar_una_pelicula)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/turpo/quispe/cinepat/PantallaInicio.kt
@@ -0,0 +1,47 @@
+package turpo.quispe.cinepat
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+
+class PantallaInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pantalla_inicio)
+
+
+        val imageView: ImageView = findViewById(R.id.perfilImageView)
+        imageView.setOnClickListener {
+            val intent = Intent(this, MiPerfil::class.java)
+            startActivity(intent)
+        }
+        val generobtn: Button =findViewById(R.id.genero)
+        generobtn.setOnClickListener {
+            val intent: Intent = Intent(this, PeliculasPorGenero::class.java)
+            startActivity(intent)
+        }
+
+        val fechaDeEmisionbtn: Button =findViewById(R.id.fecha_de_emision)
+        fechaDeEmisionbtn.setOnClickListener {
+            val intent: Intent = Intent(this, PeliculasPorFechaDeEmision::class.java)
+            startActivity(intent)
+        }
+
+        val favoritosbtn: Button =findViewById(R.id.favoritos)
+        favoritosbtn.setOnClickListener {
+            val intent: Intent = Intent(this, PeliculasPorFavoritos::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
