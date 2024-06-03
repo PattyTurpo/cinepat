19f30120e70d0528b65102e45d9d9facc75ece13
PattyTurpo <71868083@est.unap.edu.pe>
2024-06-03 02:39:51 -0500

    mi primer proyecto

--- app/src/main/java/turpo/quispe/cinepat/MiPerfil.kt
+++ app/src/main/java/turpo/quispe/cinepat/MiPerfil.kt
@@ -1,6 +1,8 @@
 package turpo.quispe.cinepat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,22 @@ class MiPerfil : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_mi_perfil)
+
+        val imageView: ImageView = findViewById(R.id.imageView133)
+        imageView.setOnClickListener {
+            val intent = Intent(this, AgregarUnaPelicula::class.java)
+            startActivity(intent)
+        }
+        val menuImageView: ImageView = findViewById(R.id.menuImageView)
+        menuImageView.setOnClickListener {
+            val intent = Intent(this, Configuracion::class.java)
+            startActivity(intent)
+        }
+        val homeImageView: ImageView = findViewById(R.id.homeImageView)
+        homeImageView.setOnClickListener {
+            val intent = Intent(this, PantallaInicio::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/turpo/quispe/cinepat/PantallaSuscripcion.kt
+++ app/src/main/java/turpo/quispe/cinepat/PantallaSuscripcion.kt
@@ -1,6 +1,8 @@
 package turpo.quispe.cinepat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,11 @@ class PantallaSuscripcion : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pantalla_suscripcion)
+        val btn: Button =findViewById(R.id.loginButton)
+        btn.setOnClickListener {
+            val intent: Intent = Intent(this, PantallaInicio::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
